22d0abcd6004098e745c03cca0bbc17494ce5f0e
Forcasius <forcasius_github@arcor.de>
2016-10-14 18:33:24 +0200

    tab -> spaces; using automatic code style;

--- ProjectDark/Assets/PadBasedController.cs
+++ ProjectDark/Assets/PadBasedController.cs
@@ -2,45 +2,48 @@
 
 public class PadBasedController : MonoBehaviour
 {
-    public float speed = 6f;            // The speed that the player will move at.
+    public float speed = 6f;
+    // The speed that the player will move at.
     public string playerAxisHorizontal;
     public string playerAxisVertical;
 
     public string lookHorizontal;
     public string lookVertical;
 
-    Vector3 movement;                   // The vector to store the direction of the player's movement.
-    Rigidbody playerRigidbody;          // Reference to the player's rigidbody.
+    Vector3 movement;
+    // The vector to store the direction of the player's movement.
+    Rigidbody playerRigidbody;
+    // Reference to the player's rigidbody.
 
-    void Awake()
+    void Awake ()
     {
         // Set up references.
-        playerRigidbody = GetComponent<Rigidbody>();
+        playerRigidbody = GetComponent<Rigidbody> (); 
     }
 
-    void FixedUpdate()
+    void FixedUpdate ()
     {
-		var newDirection = (Vector3.right * Input.GetAxis(playerAxisHorizontal) + Vector3.forward * Input.GetAxis(playerAxisVertical)).normalized;    
-		//Debug.logger.Log ("LookHorizontal", Input.GetAxisRaw (lookHorizontal));
-		//Debug.logger.Log ("LookVertical", Input.GetAxisRaw (lookVertical));
-		var newLookingDirection = new Vector3( Input.GetAxis(lookHorizontal), 0.0f, Input.GetAxis(lookVertical));
-		//newLookingDirection.Normalize();
-		if (!newLookingDirection.Equals (Vector3.zero)) 
-		{
-			Turning(newLookingDirection);
-		}
-        Move(newDirection);
+        var newDirection = (Vector3.right * Input.GetAxis (playerAxisHorizontal) + Vector3.forward * Input.GetAxis (playerAxisVertical)).normalized;    
+        //Debug.logger.Log ("LookHorizontal", Input.GetAxisRaw (lookHorizontal));
+        //Debug.logger.Log ("LookVertical", Input.GetAxisRaw (lookVertical));
+        var newLookingDirection = new Vector3 (Input.GetAxis (lookHorizontal), 0.0f, Input.GetAxis (lookVertical));
+        //newLookingDirection.Normalize();
+        if (!newLookingDirection.Equals (Vector3.zero))
+        {
+            Turning (newLookingDirection);
+        }
+        Move (newDirection);
     }
 
-    void Move(Vector3 newDirection)
+    void Move (Vector3 newDirection)
     {
         transform.position += newDirection * speed * Time.deltaTime;
     }
 
     // Update is called once per frame
-    void Turning(Vector3 newDirection)
+    void Turning (Vector3 newDirection)
     {
-		//Debug.logger.Log ("new vector", newDirection);
-		transform.rotation = Quaternion.LookRotation(newDirection);
+        //Debug.logger.Log ("new vector", newDirection);
+        transform.rotation = Quaternion.LookRotation (newDirection);
     }
 }
