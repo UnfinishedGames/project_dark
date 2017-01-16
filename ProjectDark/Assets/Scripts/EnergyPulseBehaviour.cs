bbde0c41b38baf322b7ffea22b045aae249b47e9
Forcasius <forcasius_github@arcor.de>
2017-01-16 18:17:19 +0100

    Issue #20: adding missing files that where refactored and have not been recognized automatically by git; adding EnergyPulse material - EnergyPulse uses an own material now;

---
+++ ProjectDark/Assets/Scripts/EnergyPulseBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EnergyPulseBehaviour : MonoBehaviour
+{
+    public AudioClip shootSound;
+
+    void Start()
+    {
+        Destroy(gameObject, 0.5f);
+    }
+
+    void Awake()
+    {
+        AudioSource.PlayClipAtPoint(shootSound, transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody targetRigidbody = other.GetComponent<Rigidbody>();
+        if (targetRigidbody != null)
+        {
+            PlayerHealth player = targetRigidbody.GetComponent<PlayerHealth>();
+            if (player != null)
+            {
+                player.TakeDamage();
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
