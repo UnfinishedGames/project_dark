4a87da1b815c7cd20355aa124fd22b48bed7f6d1
Forcasius <forcasius_github@arcor.de>
2017-01-24 20:15:57 +0100

    Laser lights The laser is glowing now! Awesome!

--- ProjectDark/Assets/Scripts/Weapons/LaserGunBehaviour.cs
+++ ProjectDark/Assets/Scripts/Weapons/LaserGunBehaviour.cs
@@ -10,6 +10,8 @@ public class LaserGunBehaviour : Weapon
     private float hitsPerSecond = 1.0f;
     private float timeSinceLastDamage = 0.0f;
     private LineRenderer laserLine = null;
+    private GameObject sourceLight = null;
+    private GameObject hitLight = null;
 
     void Start()
     {
@@ -27,6 +29,7 @@ public class LaserGunBehaviour : Weapon
         if (Physics.Raycast(transform.position, transform.forward, out rayCastHit))
         {
             drawLaserLine(transform.position, rayCastHit.point);
+            lightUp(transform.position, rayCastHit.point);
             //Quaternion.FromToRotation(Vector3.up, rayCast.normal);
             PlayerHealth player = rayCastHit.collider.GetComponent<PlayerHealth>();
             if (player != null)
@@ -56,12 +59,55 @@ public class LaserGunBehaviour : Weapon
         laserLine.SetPositions(points);
     }
 
+    private void lightUp(Vector3 startPosition, Vector3 endPosition)
+    {
+        if (findLights())
+        {
+            sourceLight.transform.position = startPosition;
+            sourceLight.GetComponent<Light>().enabled = true;
+            hitLight.transform.position = endPosition;
+            hitLight.GetComponent<Light>().enabled = true;
+        }
+    }
+
+    private void lightOff()
+    {
+        if (findLights())
+        {
+            sourceLight.GetComponent<Light>().enabled = false;
+            hitLight.GetComponent<Light>().enabled = false;
+        }
+    }
+
+    private bool findLights()
+    {
+        if (sourceLight == null || hitLight == null)
+        {
+            if (this.laserLine)
+            {
+                foreach (Transform attachedObject in this.laserLine.gameObject.transform)
+                {
+                    if (attachedObject.gameObject.name == "sourceLight") // TODO: is there a better way?
+                    {
+                        sourceLight = attachedObject.gameObject;
+                    }
+                    else if (attachedObject.gameObject.name == "hitLight")
+                    {
+                        hitLight = attachedObject.gameObject;
+                    }
+                }
+            }
+        }
+        return sourceLight != null && hitLight != null;
+    }
+
     public override void stop()
     {
         if (this.laserLine)
         {
             this.laserLine.enabled = false;
         }
+        lightOff();
     }
 
     public override void select()
